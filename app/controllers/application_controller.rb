8a6878b94f4f7e364bf46fafb3af2affe5f0f30d
DoanhPham <phamduydoanh@gmail.com>
2017-11-09 08:08:10 +0700

    Finish Chapter4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-   def home;end
-
-   def help;end
-
-   def about;end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
-=======
+
   root "static_pages#home"
 
   get "static_pages/home"
@@ -13,6 +9,4 @@ Rails.application.routes.draw do
   get "static_pages/about"
 
   get "static_pages/contact"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> 18791fccff8264e959c817ab7810221d089c437a
 end
