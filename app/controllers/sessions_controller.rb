716f133976156ec939aeb34154a1cc0cb7b9e972
DuyDoanh <phamduydoanh@gmail.com>
2017-11-15 16:39:31 +0700

    Finish Chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,27 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+      check_remember user
     else
-      flash.now[:danger] = I18n.t "controllers.sessions_controller.danger_invalid"
-      render :new
+      flash_create_error
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def check_remember user
+    log_in user
+    params[:session][:remember_me] == Settings.sessions_controller.bit ? remember(user) : forget(user)
+    redirect_to user
+  end
+
+  def flash_create_error
+    flash.now[:danger] = t "controllers.sessions_controller.danger_invalid"
+    render :new
   end
 end
